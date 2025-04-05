3352d969cabda9e7d60e4b49f25d2ecd4dbc1c0f
Andrei Dyldin <and@cesbo.com>
2025-04-06 01:40:19 +0300

    print version info

--- cmd/fugo/main.go
+++ cmd/fugo/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log/slog"
+	"os"
 )
 
+var Version = "0.0.0"
+
 func main() {
-	fmt.Println("Hello from Fugo!")
+	versionFlag := flag.Bool("version", false, "Print version information")
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Printf("fugo version %s\n", Version)
+		os.Exit(0)
+	}
+
+	handler := slog.NewTextHandler(os.Stdout, nil)
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
 }
